4c3231df4ae52036fc98475415a2385a2191cbb7
kimgerty <kimgerty@Kims-MacBook-Pro.local>
2017-01-19 11:25:26 +1100

    version 1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(start);
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
